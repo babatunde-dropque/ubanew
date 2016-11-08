d458825d2a1ec013a8ccb82b30e1eb767dea689a
Muyide Ibukun <superibk@gmail.com>
2016-11-08 18:31:39 +0100

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161108131930) do
 
   # These are extensions that must be enabled in order to support this database
@@ -89,14 +88,6 @@ ActiveRecord::Schema.define(version: 20161108131930) do
 
   add_index "companies", ["slug"], name: "index_companies_on_slug", unique: true, using: :btree
 
-<<<<<<< HEAD
-  create_table "contacts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "attachment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "company_id"
-=======
   create_table "contact_forms", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -104,7 +95,14 @@ ActiveRecord::Schema.define(version: 20161108131930) do
     t.string   "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
->>>>>>> a54b3a42efef5ec1b3ab705b2ca3fb7e5ea02c4b
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "company_id"
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
