ac432b7d1a4554b759a1545399daa1157623603e
Muyide Ibukun <superibk@gmail.com>
2016-11-22 11:04:55 +0100

    remote https from staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,10 +57,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  if ENV['STAGE'] == "staging"
-      config.force_ssl = false
-  elsif ENV['STAGE'] == "live"
-    config.force_ssl = true
+  if ENV['STAGE'] == "live"
+      config.force_ssl = true
   end
 
   # Use the lowest log level to ensure availability of diagnostic information
