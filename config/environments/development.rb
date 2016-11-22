8c72da4f1f316d0797892a48931a52d69671d34a
Muyide Ibukun <superibk@gmail.com>
2016-11-22 23:15:57 +0100

    correct mailer issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,16 +18,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   #Change mail delivery to either :smtp, :sendmail, :file, :test
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: 'smtp.sendgrid.net',
-      port: 587,
-      domain: 'www.dropque.com',
-      user_name: ENV["SENDGRID_USERNAME"],
-      password:  ENV["SENDGRID_PASSWORD"],
-      authentication: 'plain',
-      enable_starttls_auto: true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #     address: 'smtp.sendgrid.net',
+  #     port: 587,
+  #     domain: 'www.dropque.com',
+  #     user_name: ENV["SENDGRID_USERNAME"],
+  #     password:  ENV["SENDGRID_PASSWORD"],
+  #     authentication: 'plain',
+  #     enable_starttls_auto: true
+  # }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
