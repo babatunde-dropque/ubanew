396b06c998d776243bfaf169d6cbae7b97ca5624
Muyide Ibukun <superibk@gmail.com>
2016-11-22 13:04:04 +0100

    remove staging file and use SKIP_FORCE_SSL=true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   
-  config.force_ssl = true
+  # config.force_ssl = true
+  config.force_ssl = true unless ENV['SKIP_FORCE_SSL']
  
 
   # Use the lowest log level to ensure availability of diagnostic information
