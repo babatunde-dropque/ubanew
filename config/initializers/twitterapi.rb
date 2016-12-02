994e28c952b2dcd37592ca78fe5a2cd40082a5d1
Sulphur1010 <mustaphaalade@gmail.com>
2016-12-02 15:19:54 +0100

    create interview_expire page

---
+++ config/initializers/twitterapi.rb
@@ -0,0 +1,8 @@
+require 'twitter'
+
+ client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "GODTPxHbTwP6gx3fQgoXKqiFa"
+  config.consumer_secret     = "I80yAT36j37ols7zvPYYU3XqHweOD87e7iOJiCt7v4bXOUZcPr"
+  config.access_token        = "740225257785729025-MVZ8iJQxFvfdQbaTe5kj0vijtRaYS4i"
+  config.access_token_secret = "rBhnic8XXwV6s9OqbSzAMTVlgOgDTug1PPfbDM97lKX7c"
+end
