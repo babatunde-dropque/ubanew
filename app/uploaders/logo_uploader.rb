7a439cde15dc5423f7e8788beb9a6c3910182bec
Muyide Ibukun <superibk@gmail.com>
2016-12-13 17:12:53 +0100

    add live chat to app

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -38,7 +38,12 @@ class LogoUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
-  process resize_to_fit: [231, 80]
+ 
+  # returns an image with a maximum width of 100px 
+  # while maintaining the aspect ratio
+  # 10000 is used to tell CW that the height is free 
+  # and so that it will hit the 100 px width first
+  process :resize_to_fit => [10000, 80]
   # def scale(width, height)
   #   # do something
   # end
