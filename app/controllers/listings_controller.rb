2e7ad99097707bc51881d37628174de6198927cf
Muyide Ibukun <superibk@gmail.com>
2017-02-17 00:58:04 +0100

    finish up interview listing page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,8 +3,19 @@ class ListingsController < ApplicationController
 
 	# this code only gets
 	def listing_interview
-		if params[:search]
-		   @interviews = Interview.search(params[:search]).paginate(:page => params[:page], :per_page => 5).order("created_at DESC")
+		if params[:search] && request.subdomain.present?
+
+		   @company = Company.find_by(subdomain:request.subdomain )
+		   interviews_all = @company.interviews
+		   @interviews = interviews_all.where("title LIKE ?", "%#{params[:search]}%").paginate(:page => params[:page], :per_page => 5).order("created_at DESC")
+		   
+		elsif  request.subdomain.present?
+    		 @company = Company.find_by(subdomain: request.subdomain)
+    		 @interviews = @company.interviews.paginate(:page => params[:page], :per_page => 5).order("created_at DESC")
+		
+		elsif params[:search]
+			 @interviews = Interview.search(params[:search]).paginate(:page => params[:page], :per_page => 5).order("created_at DESC")
+		
 		else
 		  @interviews = Interview.paginate(:page => params[:page], :per_page => 5).order('created_at DESC')
 		end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215144952) do
+ActiveRecord::Schema.define(version: 20170216133702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20170215144952) do
   end
 
   add_index "companies", ["slug"], name: "index_companies_on_slug", unique: true, using: :btree
+  add_index "companies", ["subdomain"], name: "index_companies_on_subdomain", unique: true, using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
@@ -252,7 +253,6 @@ ActiveRecord::Schema.define(version: 20170215144952) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
-    t.string   "package"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
