19f7b95e83c122cf63d4f234c6ccf51e289e2451
Muyide Ibukun <superibk@gmail.com>
2017-02-17 08:32:41 +0100

    exempt www from listing domain

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,13 +3,13 @@ class ListingsController < ApplicationController
 
 	# this code only gets
 	def listing_interview
-		if params[:search] && request.subdomain.present? && request.subdomain != "wwww"
+		if params[:search] && request.subdomain.present? && request.subdomain != 'wwww'
 
 		   @company = Company.find_by(subdomain:request.subdomain )
 		   interviews_all = @company.interviews
 		   @interviews = interviews_all.where("title LIKE ?", "%#{params[:search]}%").paginate(:page => params[:page], :per_page => 5).order("created_at DESC")
 		   
-		elsif  request.subdomain.present? && request.subdomain != "wwww"
+		elsif  request.subdomain.present? && request.subdomain != 'wwww'
     		 @company = Company.find_by(subdomain: request.subdomain)
     		 @interviews = @company.interviews.paginate(:page => params[:page], :per_page => 5).order("created_at DESC")
 		
