9907f18f049f5c44ab5be31ac838ca470b871381
Sulphur1010 <mustaphaalade@gmail.com>
2017-06-01 16:06:11 +0100

    add device column to submissions table

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -107,6 +107,15 @@ end
   end
 
 
+  def mobile_device?
+  if session[:mobile_param]
+    session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
+
   # this is a stand alone api
   def submit_video
     # am not just using Submission.find, so that any one can't just modify the submission
