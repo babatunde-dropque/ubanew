42106ab810e6d5539b8c41ebc96982eac9f1973d
Yaw Boakye <wheresyaw@gmail.com>
2017-06-12 10:03:20 +0000

    Use Puma application server
    Webrick is a development server, not to be used in production. See
    http://puma.io/ for more information on Puma and how to configure it.

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,8 +1,8 @@
 #  added by muyide ibukun, to change the behaviour of invitable controller
 class Users::InvitationsController < Devise::InvitationsController
   layout 'signin'
-   skip_filter :require_no_authentication, :only => :edit
-   before_action :devise_configure_permitted_parameters, if: :devise_controller?
+  skip_filter :require_no_authentication, :only => :edit
+  before_action :devise_configure_permitted_parameters, if: :devise_controller?
   
 
   protected
