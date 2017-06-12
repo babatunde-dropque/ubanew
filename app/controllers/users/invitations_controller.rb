b2e77ae235beaf6fbea87c3b31743dda36e015db
Yaw Boakye <wheresyaw@gmail.com>
2017-06-12 09:03:34 +0000

    Update changed API for devise_parameter_sanitizer
    See https://github.com/plataformatec/devise#strong-parameters for
    details.

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -4,18 +4,11 @@ class Users::InvitationsController < Devise::InvitationsController
    skip_filter :require_no_authentication, :only => :edit
    before_action :devise_configure_permitted_parameters, if: :devise_controller?
   
+
   protected
 
-
-
-
-  def devise_configure_permitted_parameters
-  	if user_signed_in?
-      sign_out current_user
+    def devise_configure_permitted_parameters
+      sign_out current_user if user_signed_in?
+      devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name])
     end
-
-    devise_parameter_sanitizer.for(:accept_invitation) << :name
-  end
-  
-
 end
