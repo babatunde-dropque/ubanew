82dc4c13dbbeb1972e965b5544d7d9366d91c060
Sulphur1010 <mustaphaalade@gmail.com>
2017-08-07 17:24:32 +0100

    separate company navbar from talent_show view

--- db/schema.rb
+++ db/schema.rb
@@ -289,6 +289,7 @@ ActiveRecord::Schema.define(version: 20170801085555) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
+    t.string   "package"
     t.string   "telephone"
     t.string   "a_qualification"
     t.integer  "a_experience"
